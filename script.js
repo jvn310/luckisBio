1526ef97d94334760826d463e581f965c912e2c0
jvn3 <132863300+stillmste3@users.noreply.github.com>
2024-05-11 10:07:54 +0200

    added a row and read more/read less texts using CSS and Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const parentContainer = document.querySelector('.container');
+parentContainer.addEventListener('click', event => {
+const current = event.target;
+const isReadMoreBtn = current.className.includes('read-more-btn');
+if (!isReadMoreBtn) return;
+const currentText = event.target.parentNode.querySelector('.read-more-text');
+currentText.classList.toggle('read-more-text--show');
+current.textContent = current.textContent.includes('Read More...') ?
+"Read Less..." : "Read More....";
+})
